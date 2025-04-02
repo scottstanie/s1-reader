d6d8a2f7920d7b54b85faf741a1e3fd366c6fd86
Scott Staniewicz <scott.stanie@gmail.com>
2025-04-01 21:21:14 -0400

    Set S1C relative orbit offset to 172
    Closes #141

--- src/s1reader/s1_burst_slc.py
+++ src/s1reader/s1_burst_slc.py
@@ -245,7 +245,7 @@ class Sentinel1BurstSlc:
     range_bandwidth: float
     polarization: str  # {VV, VH, HH, HV}
     burst_id: S1BurstId
-    platform_id: str  # S1{A,B}
+    platform_id: str  # S1{A,B,C}
     safe_filename: str  # SAFE file name
     center: tuple  # {center lon, center lat} in degrees
     border: list  # list of lon, lat coordinate tuples (in degrees) representing burst border
@@ -1180,5 +1180,5 @@ class Sentinel1BurstSlc:
         elif self.platform_id == "S1B":
             orbit_number_offset = 27
         else:
-            orbit_number_offset = 99
+            orbit_number_offset = 172
         return (self.abs_orbit_number - orbit_number_offset) % 175 + 1
